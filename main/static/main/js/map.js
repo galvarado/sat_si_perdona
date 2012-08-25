0d2e4bdf45dd563bc0bde41c519896a4fd46f9c3
Guillermo <stallion.ak47@gmail.com>
2012-08-25 16:14:25 -0500

    charts on map

--- main/static/main/js/map.js
+++ main/static/main/js/map.js
@@ -46,15 +46,48 @@ $(function() {
   // Activate map highlight funcionality
   $('#map').maphilight();
   // When user is over an state
-  $('.area').hover(function() {
+  $('.area').click(function() {
     // Obtain name of state dependind his id and put it in the info div
     var id= $(this).attr('id');
     var state = $.inArray(id,states);
     $('#info').html(state_names[state]);
-    datatable.fnFilter(state_names[state]);
-    // Show table results
-    $('#deputies-table').show();
-  }, function(){
-      $('#info').html('');
+    $.ajax({
+      url: "/get_graph_by_state",
+      type: 'post',
+      dataType: 'json',
+      data: {
+        state:state_names[state],
+      },
+      success: function(data) {
+        $("#graph-tabs").tabs().show();
+        if (data.response) {
+            var pie = new RGraph.Pie('cvs1', [data.total_moral,data.total_physic]);
+            pie.Set('chart.origin', 0);
+            pie.Set('chart.tooltips', [data.total_moral,data.total_physic]);
+            pie.Set('chart.labels', ['Fisicas','Morales']);
+            pie.Set('chart.shadow', true);            
+            pie.Set('chart.background.piecolor1', 'white');
+            pie.Set('chart.background.piecolor2', 'white');
+            pie.Set('chart.background.grid', true);
+            pie.Set('chart.colors', ['orange', 'white']);
+            pie.Set('chart.text.color', 'white');
+            pie.Set('chart.variant', '3d');
+            pie.Draw();
+            var bar = new RGraph.Bar('cvs2', [5,4,1,6,8,5,3]);
+            bar.Set('chart.labels', ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+            bar.Draw();
+            bar.Set('chart.background.barcolor1', 'white');
+            bar.Set('chart.background.barcolor2', 'white');
+            bar.Set('chart.background.grid', true);
+            bar.Set('chart.colors', ['orange', 'white']);
+            bar.Set('chart.text.color', 'white');
+            bar.Set('chart.variant', '3d');
+            bar.Draw();
+            var hbar = new RGraph.HBar('cvs3', [5,8,6,4,3,2,1,6,8]);
+            hbar.Set('chart.labels', ['Venus','Felicity','Cynthia','Frederick','Hoolio','Peter','Jennifer','Richard','Joathan']);
+            hbar.Draw();
+        }
+      }
+    });
   });
 });
